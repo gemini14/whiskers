e308ff08de969840c8f024726fe25aded9677b55
Tarik Sturm <tarik@pinguintechnik.com>
2019-04-24 22:51:14 -0700

    Add initial project files and starter content. Enable Git LFS.

---
+++ Source/whiskers/whiskersGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "whiskersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WHISKERS_API AwhiskersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
